ed854ece6b9247774a65a6176e6861c952012990
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-01-24 14:33:52 -0300

    Adicionando lombok para excluir codigo boilerplate, como getters, setters e equals e hashcode

--- src/main/java/com/api/food/domain/model/Cozinha.java
+++ src/main/java/com/api/food/domain/model/Cozinha.java
@@ -1,44 +1,19 @@
 package com.api.food.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
-
+@Data //@Getter, @Setter, @ToString, @EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table
 public class Cozinha {
+
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String nome;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(id, cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
