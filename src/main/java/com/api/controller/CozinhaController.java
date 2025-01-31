92fbe2e3564a6bfbacb471690637b7ef5a1a3ba7
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-01-31 14:15:00 -0300

    Substituindo anotacoes @Controller e @ResponseBody por @RestController

--- src/main/java/com/api/controller/CozinhaController.java
+++ src/main/java/com/api/controller/CozinhaController.java
@@ -3,15 +3,13 @@ package com.api.controller;
 import com.api.food.domain.model.Cozinha;
 import com.api.food.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 
