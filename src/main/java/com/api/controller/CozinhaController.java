a467585c274950058e6cd8627f136abff2f170b6
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-01-31 14:14:14 -0300

    Adicionando controlador para cozinha e add metodo de listar cozinhas

---
+++ src/main/java/com/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.api.controller;
+
+import com.api.food.domain.model.Cozinha;
+import com.api.food.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@ResponseBody
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
