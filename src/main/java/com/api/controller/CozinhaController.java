7bedcbf5a852f8a12ca6fbd1110e8e4e18783206
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-02-04 16:56:45 -0300

    Usando ResponseEntity para resposta de buscar

--- src/main/java/com/api/controller/CozinhaController.java
+++ src/main/java/com/api/controller/CozinhaController.java
@@ -6,6 +6,7 @@ import com.api.model.CozinhasXmlWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,7 +30,9 @@ public class CozinhaController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha =  cozinhaRepository.buscar(cozinhaId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
     }
 }
