c4ba457b6b6d0f10102844c507e1ae0836973bbb
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-02-05 09:49:06 -0300

    Retornando representacao do recurso criado

--- src/main/java/com/api/controller/CozinhaController.java
+++ src/main/java/com/api/controller/CozinhaController.java
@@ -41,7 +41,7 @@ public class CozinhaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void adicionar(@RequestBody Cozinha cozinha) {
-        cozinhaRepository.salvar(cozinha);
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
     }
 }
