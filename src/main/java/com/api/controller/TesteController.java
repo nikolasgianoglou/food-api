cc23991e66a5adadc806130a6c8e0ed5a0d8bdc6
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-02-12 10:13:40 -0300

    Usando as keywords para definir critérios de query methods

--- src/main/java/com/api/controller/TesteController.java
+++ src/main/java/com/api/controller/TesteController.java
@@ -1,14 +1,18 @@
 package com.api.controller;
 
 import com.api.food.domain.model.Cozinha;
+import com.api.food.domain.model.Restaurante;
 import com.api.food.domain.repository.CozinhaRepository;
+import com.api.food.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -17,8 +21,26 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
     @GetMapping("/cozinhas/por-nome")
-    public List<Cozinha> buscarPorNome(@RequestParam("nome") String nome) {
-        return cozinhaRepository.nome(nome);
+    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantesPorNomeECozinhaId(String nome, Long id) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, id);
     }
 }
