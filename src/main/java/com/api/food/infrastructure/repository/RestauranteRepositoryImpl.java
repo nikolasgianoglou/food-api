59e55450d1fcf10557c3edc7c823e9cb16edc246
nikolasgianoglou <nikolasgcoelho@gmail.com>
2025-02-17 15:01:29 -0300

    Implementando uma consulta dinâmica com JPQL

--- src/main/java/com/api/food/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/api/food/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -4,9 +4,12 @@ import com.api.food.domain.model.Restaurante;
 import com.api.food.domain.repository.RestauranteRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 /*
@@ -31,6 +34,36 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
             .setParameter("taxaInicial", taxaFreteInicial)
             .setParameter("taxaFinal", taxaFreteFinal)
             .getResultList();
+    }
+
+    public List<Restaurante> findDynamic(String nome,
+                                         BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+        var jpql = new StringBuilder();
+
+        jpql.append("from Restaurante where 0 = 0 ");
+
+        var parametros = new HashMap<String, Object>();
+
+        if (StringUtils.hasLength(nome)) {
+            jpql.append("and nome like :nome ");
+            parametros.put("nome", "%" + nome + "%");
+        }
+
+        if (taxaFreteInicial != null) {
+            jpql.append("and taxaFrete >= :taxaInicial");
+            parametros. put ("taxaInicial", taxaFreteInicial);
+        }
+
+        if (taxaFreteFinal != null) {
+            jpql.append("and taxaFrete <= :taxaFinal");
+            parametros.put ("taxaFinal", taxaFreteFinal);
+        }
+
+        TypedQuery<Restaurante> query =  manager.createQuery(jpql.toString(), Restaurante.class);
+
+        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+        return query.getResultList();
     }
 
 }
